c4fe4fc9c354c450751384866d29651dafa475c5
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-27 01:39:40 +0300

    Add files via upload
    edit files with formater black

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from transformers import pipeline
 
-page_bg_img = '''
+page_bg_img = """
 <style>
 body {
 background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
 </style>
-'''
+"""
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
@@ -23,8 +23,8 @@ else:
 
 if st.button("Submit"):
     distilled_student_sentiment_classifier = pipeline(
-        model="lxyuan/distilbert-base-multilingual-cased-sentiments-student", 
-        top_k=1)
+        model="lxyuan/distilbert-base-multilingual-cased-sentiments-student", top_k=1
+    )
     # english
     score = distilled_student_sentiment_classifier(user_input)
-    st.write( "Result: ", score[0][0]['label'],score[0][0]['score'])
+    st.write("Result: ", score[0][0]["label"], score[0][0]["score"])
